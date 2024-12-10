3da91db584b7052d71dee3e6cd1f14defcd2a77c
mmanngard <40800685+mmanngard@users.noreply.github.com>
2024-12-10 15:51:11 +0200

    cooling system matlab model

---
+++ parameters.m
@@ -0,0 +1,20 @@
+clear all
+
+cp = 4190; %Specific heat capacity [J/kg/K]
+T_in = 60; % Temnperature [degC]
+
+% Engine
+M_E = 2000; %mass [kg]
+
+% Mixing node
+M_MIX = 20; %mass [kg]
+
+% Radiator
+M_R = 1000; %mass [kg]
+
+% Boiler
+M_B = 10000; %mass [kg]
+
+% initial values
+T_ini = 60;
+mdot_ini = 30; % massflow rate [kg/s]
